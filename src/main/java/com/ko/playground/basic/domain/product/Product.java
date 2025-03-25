83743793764a64a888bfb2828e0bbf13301a2258
jiwonpg98 <jiwonpg98@gmail.com>
2025-03-26 00:36:38 +0900

    feat: 도메인 추가

---
+++ src/main/java/com/ko/playground/basic/domain/product/Product.java
@@ -0,0 +1,27 @@
+package com.ko.playground.basic.domain.product;
+
+import com.ko.playground.basic.support.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class Product extends BaseEntity {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    @Column(name = "description")
+    private Long description = null;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
---
+++ src/main/java/com/ko/playground/basic/domain/productregistration/ProductRegistration.java
@@ -0,0 +1,51 @@
+package com.ko.playground.basic.domain.productregistration;
+
+import com.ko.playground.basic.support.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+public class ProductRegistration extends BaseEntity {
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "is_registered", nullable = false)
+    private Boolean isRegistered = false;
+
+    @Column(name = "parent_id", nullable = false)
+    private Long parentId;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public ProductRegistration() {
+        this.productId = getProductId();
+        this.isRegistered = getIsRegistered();
+        this.parentId = getParentId();
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public static ProductRegistration create(Long productId, Long parentId) {
+        return ProductRegistration.builder()
+            .productId(productId)
+            .parentId(parentId)
+            .build();
+    }
+
+    public void register() {
+        this.isRegistered = true;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
