5a02f51b245cfac5c7c9dbaeba71e2fafa8297bb
jiwonpg98 <jiwonpg98@gmail.com>
2025-03-28 00:55:51 +0900

    feat: 비밀번호 인코딩 부분 추가

--- src/main/java/com/ko/playground/basic/core/parent/domain/Parent.java
+++ src/main/java/com/ko/playground/basic/core/parent/domain/Parent.java
@@ -3,14 +3,18 @@ package com.ko.playground.basic.core.parent.domain;
 import com.ko.playground.basic.support.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Setter
+@Builder
 @Entity(name = "parent")
+@AllArgsConstructor
 public class Parent extends BaseEntity {
     @Column(name = "email", nullable = false)
     private String email;
@@ -18,9 +22,31 @@ public class Parent extends BaseEntity {
     @Column(name = "encrypted_password", nullable = false)
     private String encryptedPassword;
 
+    @Builder.Default
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    @Builder.Default
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt = LocalDateTime.now();
+
+    public Parent() {
+        this.email = getEmail();
+        this.encryptedPassword = getEncryptedPassword();
+        this.createdAt = getCreatedAt();
+        this.updatedAt = getUpdatedAt();
+    }
+
+    public static Parent create(String email, String encryptedPassword) {
+        return Parent.builder()
+                .email(email)
+                .encryptedPassword(encryptedPassword)
+                .build();
+    }
+
+    void verifyPassword(PasswordEncoder passwordEncoder, String password) {
+        if (!passwordEncoder.matches(password, encryptedPassword)) {
+            throw new IllegalArgumentException("Password is not correct");
+        };
+    }
 }
